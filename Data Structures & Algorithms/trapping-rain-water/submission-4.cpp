022f736ad3cf322abbe604eeefcd52a00bd1e3e3
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-04-09 21:52:28 +0300

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftMax = height.front();
+        int rightMax = height.back();;
+        int left = 0;
+        int right = height.size() - 1;
+
+        int ans = 0;
+
+        while (left < right) {
+            int minLimit = min(leftMax, rightMax);
+            int currentDelta = 0;
+            if (leftMax < rightMax) {
+                left++;
+                currentDelta = minLimit - height[left];
+                leftMax = max(leftMax, height[left]);
+            } else {
+                right--;
+                currentDelta = minLimit - height[right];
+                rightMax = max(rightMax, height[right]);
+            }
+            if (currentDelta > 0) {
+                ans += currentDelta;
+            }
+        }
+        return ans;
+    }
+};
