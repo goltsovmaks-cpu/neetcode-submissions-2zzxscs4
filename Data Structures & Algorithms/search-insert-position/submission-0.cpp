2dcf5997fdfb35d4a86aa51f2a72ea3442f7de76
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-04-11 19:58:12 +0300

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2; 
+            int item = nums[mid];
+            if (item < target) {
+                left = mid + 1;
+            } else if (item > target) {
+                right = mid;
+            } else { 
+                return mid;
+            }
+        }
+        return left;
+    }
+};
