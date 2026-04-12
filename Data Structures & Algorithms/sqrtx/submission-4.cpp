b0ccc60a388f7ead1a098ce6994b02ce6a752492
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-04-12 15:01:53 +0300

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+
+        int BOUND = 46340;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid > BOUND) { // to big for 2^32.
+                right = mid - 1;
+                continue;
+            }
+
+            int pow2 = mid * mid;
+            int distance = x - pow2;
+            int deltaBetweenNexNum = mid + (mid+1); // (mid+1) ^ 2;
+            if (0 <= distance && distance < deltaBetweenNexNum) {
+                return mid; 
+            } else if (distance < 0)  { // to big
+                right = mid - 1;
+            } else { // to smal 
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
