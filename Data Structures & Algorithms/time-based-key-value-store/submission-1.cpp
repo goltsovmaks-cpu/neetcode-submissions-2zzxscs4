e7c51ffa6e7a6223cbc55ce87f47c2d38e823af7
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-04-16 20:42:24 +0300

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,55 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        auto& values = Storage_[key];
+        if (values.empty() || values.back().second != value) {
+            values.emplace_back(timestamp, value);
+        }
+    }
+    
+    string get(string key, int timestamp) {
+        auto valuesIt = Storage_.find(key);
+        if (Storage_.end() == valuesIt || valuesIt->second.front().first > timestamp) {
+            return "";
+        }
+
+        const auto& values = valuesIt->second;
+
+        int l = 0;
+        int r = values.size() - 1;
+
+        int idx = l;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (values[m].first == timestamp) {
+                idx = m;
+                break;
+            }
+
+            if (values[m].first < timestamp) {
+                idx = max(idx, m);
+                l = m + 1;
+            } else { 
+                r = m - 1;
+            }
+        }
+
+        return values[idx].second;
+
+        // auto upper = std::ranges::upper_bound(valuesIt->second, timestamp,
+        //     std::ranges::less{},
+        //     [](const TVersionedValue& v) { return v.first; });
+
+        // upper--;
+        // return upper->second;
+    }
+
+private:
+    using TVersionedValue = pair<int, string>;
+    unordered_map<string, vector<TVersionedValue>> Storage_;
+};
