7f354e5424edf11eea7aef720212741b4a5f33be
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-06-22 23:09:27 +0300

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> path;
+        vector<vector<string>> result;
+        dfs(0, n, path, result);
+        return result;
+    }
+
+    void dfs(int runkNum, int n, vector<int>& path, vector<vector<string>>& out) {
+        if (runkNum == n) {
+            if (path.size() == n) {
+                vector<string> position;
+                position.reserve(n);
+                for (int i = 0; i < n; i++) {
+                    string rank(n, '.');
+                    rank[path[i]] = 'Q';
+                    position.push_back(rank);
+                }
+                out.push_back(position);
+            }
+            return;
+        }
+
+        // iterate over rank!
+        for (int currentFile = 0; currentFile < n; currentFile++) {
+            bool skip = false;
+            for (int i = 0; i < path.size(); i++) {
+                int pathFile = path[i];
+                int horizontalDistance = abs(currentFile - pathFile);
+                int verticalDistance = abs(runkNum - i);
+
+                if (currentFile == pathFile || horizontalDistance == verticalDistance) {
+                    skip = true;
+                    break;
+                }
+            }
+
+            if (skip) {
+                continue;
+            }
+
+            path.push_back(currentFile);
+            dfs(runkNum + 1, n, path, out);
+            path.pop_back();
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
