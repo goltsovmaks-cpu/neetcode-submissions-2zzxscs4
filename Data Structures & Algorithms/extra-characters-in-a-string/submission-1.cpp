a89aaf6f94fb07695c06a94da3952f37f058304e
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-06-29 20:04:39 +0300

    Add: extra-characters-in-a-string - submission-1

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+
+        vector<optional<int>> cache(s.size() + 1);
+        cache[s.size()] = 0;
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
+        return dfs(s, 0, dict, cache);
+    }
+
+    int dfs(
+        const string& s,
+        int startIndex,
+        const unordered_set<string>& dict,
+        vector<optional<int>>& cache)
+    {
+        if (cache[startIndex].has_value()) {
+            return *cache[startIndex];
+        }
+
+        int result = 1 + dfs(s, startIndex + 1, dict, cache);
+        for (int i = startIndex; i < s.size(); i++) {
+            string substr = s.substr(startIndex, i - startIndex + 1);
+            if (!dict.contains(substr)) {
+                continue;
+            }
+            result = min(result, dfs(s, startIndex + substr.size(), dict, cache));
+        }
+        cache[startIndex] = result;
+        return result;
+    }
+};
