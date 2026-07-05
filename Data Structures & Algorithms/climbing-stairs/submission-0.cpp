bd8465879a0ff0ff6b6ab634e98651cab9db48bc
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-07-05 19:25:55 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int prevPrev = 1;
+        int prev = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int current = prev + prevPrev;
+            prevPrev = prev;
+            prev = current;
+        }
+        return prev;
+    }
+};
