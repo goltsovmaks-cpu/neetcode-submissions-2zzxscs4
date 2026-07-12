c5d13e3779abeba86f93ed9524067d2e1e74ad74
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-07-12 16:17:51 +0300

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin: coins) {
+                if (i - coin >= 0) {
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return amount + 1 == dp.back() ? -1 : dp.back();
+    }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     if (0 == amount) {
+    //         return 0;
+    //     }
+    //     vector<int> dp(amount, 0);
+    //     dp[0] = -1;
+
+    //     for (int i = amount - 1; i >=0; i--) {
+    //         for (int coin: coins) {
+    //             int sum = i + coin;
+    //             if (amount == sum) {
+    //                 dp[i] = 1;
+    //                 break;
+    //             } else if (sum > amount) {
+    //                 continue;
+    //             } else if (dp[sum]) {
+    //                 int currentBestCount = 1 + dp[sum];
+    //                 dp[i] = dp[i] > 0 ? min(dp[i], currentBestCount) : currentBestCount;
+    //             }
+    //         }
+    //     }
+
+    //     return dp.front();
+    // }
+};
