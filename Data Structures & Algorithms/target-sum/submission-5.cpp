6507af57b57679713f18321c87b025970994c553
goltsovmaks-cpu <goltsovmaks@gmail.com>
2026-08-03 21:07:55 +0300

    Add: target-sum - submission-5

---
+++ Data Structures & Algorithms/target-sum/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // index -> Ith num, 
+        // map<sum, countOfWays>
+        TCache cache(nums.size());
+        int res =  dfs(nums, 0, target, cache);
+        return res;
+    }
+private:
+    using TCache = vector<unordered_map<int, int>>;
+    int dfs(vector<int>& nums, int i, int target, TCache& cache) {
+        if (nums.size() == i) {
+            return 0 == target ? 1 : 0;
+        }
+        if (cache[i].contains(target)) {
+            return cache[i][target];
+        }
+        int num = nums[i];
+
+        cache[i][target] = 
+            dfs(nums, i + 1, target - num, cache) +
+            dfs(nums, i + 1, target + num, cache);
+
+
+        return cache[i][target];
+    }
+};
